b068b1d97a3e352cdd9d0bd087bb9e0e1b3309f9
Denis <ddujota@internationalsafety.com>
2019-08-16 01:36:05 -0400

    ran build and added the configs required to have the react app load up on local host 8000 via proxy

--- reactDjangoApi/views.py
+++ reactDjangoApi/views.py
@@ -1,5 +1,5 @@
 import logging
-# import os
+import os
 
 from django.views.generic import View
 from django.http import HttpResponse
@@ -7,22 +7,10 @@ from django.conf import settings
 
 
 class ReactAppMountingPoint(View):
-    """
-    Serves the compiled frontend entry point (only works if you have run `yarn
-    run build`).
-    """
-
     def get(self, request):
         try:
             with open(os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')) as f:
                 return HttpResponse(f.read())
         except FileNotFoundError:
             logging.exception('Production Build of App not found')
-            return HttpResponse(
-                """
-                This URL is only used when you have built the production
-                version of the app. Visit http://localhost:3000/ instead, or
-                run `yarn run build` to test the production version.
-                """,
-                status=501,
-            )
+            return HttpResponse(status=501)
